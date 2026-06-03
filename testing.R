321a45c0f94de36c6f8a0588d945d95fb30d70e6
tylerstoen <tyler.stoen@gmail.com>
2026-06-03 12:33:24 -0700

    simplify testing function

--- testing.R
+++ testing.R
@@ -7,28 +7,18 @@ library(MASS)
 library(mvtnorm)
 
 treatment_data_generation_3d <- function(rows, cols, A_cov, B_cov, C_cov, test_group) {
-  sigma_A <- diag(rows)  # makes a 100x100 identity matrix
-  sigma_A[test_group, test_group] <- A_cov #gives the first 10 rows positive correlations
-  sigma_A[row(sigma_A) == col(sigma_A)] = 1 # makes sure diagonal is one
   
-  sigma_B <- diag(rows)
-  sigma_B[test_group,test_group] <- B_cov
-  sigma_B[row(sigma_B) == col(sigma_B)] = 1
+  make_sigma <- function(cov_val) {
+    sigma <- diag(rows)
+    sigma[test_group, test_group] <- cov_val
+    diag(sigma) <- 1
+    sigma
+  }
   
-  sigma_C <- diag(rows)
-  sigma_C[test_group,test_group] <- C_cov
-  sigma_C[row(sigma_C) == col(sigma_C)] = 1
-  
-  sample_A <- t(mvrnorm(cols, rep(0,rows), sigma_A))
-  sample_B <- t(mvrnorm(cols, rep(0,rows), sigma_B))
-  sample_C <- t(mvrnorm(cols, rep(0,rows), sigma_C))
-  
-  test_dfs <- list(
-    data.frame(sample_A, row.names = paste0("V", 1:rows)), 
-    data.frame(sample_B, row.names = paste0("V", 1:rows)), 
-    data.frame(sample_C, row.names = paste0("V", 1:rows))
-  )
-  return(test_dfs)
+  lapply(c(A_cov, B_cov, C_cov), function(cov_val) {
+    samples <- t(mvrnorm(cols, rep(0, rows), make_sigma(cov_val)))
+    data.frame(samples, row.names = paste0("Item", 1:rows))
+  })
 }
 
 treatment_data_generation_bg <- function(rows, cols, A_cov, B_cov, C_cov, 
